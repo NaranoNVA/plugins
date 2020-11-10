56db4cc49bd50ad1ab456d68c3d461df446933d7
Nicolas Alberto <darkathnva@gmail.com>
2020-11-10 20:41:46 -0300

    Puxando dados do post atual e colocando no head

--- primeiro_puglin_N/primeiro_pulgin_N.php
+++ primeiro_puglin_N/primeiro_pulgin_N.php
@@ -9,9 +9,32 @@
 * License: CC BY
 */
 
-add_filter( 'login_errors', 'altera_msg_login' );
+add_filter( 'login_errors', 'altera_msg_login' );   
 
 function altera_msg_login(){
 
     return 'Credenciais inválidas';
+}
+
+add_action( 'wp_head', 'colocar_og_tags' );
+
+function colocar_og_tags(){
+
+    if( is_single() ){
+
+        $dados_post = get_post(get_the_ID());
+        $nome_site = get_bloginfo();
+        $titulo = $dados_post->post_title;
+        $resumo = $dados_post->post_excerpt;
+        
+
+        echo "  \n\n\n
+                <meta property='og:title' content='" . $titulo . "'>\n
+                <meta property='og:site_name' content='" . $nome_site  . "'>\n
+                <meta property='og:url' content='" . get_permalink() . "'>\n
+                <meta property='og:description' content='" . $resumo . "'>\n
+                \n\n\n";
+
+    }
+
 }
